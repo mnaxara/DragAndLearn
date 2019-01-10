1658d13686fff167183a07ea5fb775b0bf6ed17e
mnaxara <45257561+mnaxara@users.noreply.github.com>
2019-01-10 18:56:41 +0100

    securityAndBase

---
+++ src/Controller/ExerciceController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ExerciceController extends AbstractController
+{
+    /**
+     * @Route("/exercice", name="exercice")
+     */
+    public function index()
+    {
+        return $this->render('exercice/index.html.twig', [
+            'controller_name' => 'ExerciceController',
+        ]);
+    }
+}
