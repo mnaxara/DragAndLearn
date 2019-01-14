2f0daf319d5227778a17f00abe19ec22c9373a3d
Darpen <ben.peyrat@gmail.com>
2019-01-14 09:20:09 +0100

    update monday morning

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -21,7 +21,6 @@ class HomeController extends AbstractController
      */
     public function levelChoice()
     {
-        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         return $this->render('home/levelChoice.html.twig');
     }
