154a8953abd3740f828bf9f7f2e1a67bb2e53e50
Darpen <ben.peyrat@gmail.com>
2019-01-17 16:21:56 +0100

    adminUser

--- src/Controller/AjaxController.php
+++ src/Controller/AjaxController.php
@@ -6,7 +6,9 @@ use App\Entity\User;
 use App\Form\UserType;
 use App\Entity\Exercice;
 use App\Entity\UserHasExercices;
+use App\Service\FileUploader;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
@@ -57,9 +59,10 @@ class AjaxController extends AbstractController
         //on remplace le nom du fichier par un objet de classe File
         //pour pouvoir générer le formulaire
         if($user->getAvatar()){
-            $user->setAvatar(new File($this->getParameter('upload_directory') . $this->getParameter('article_image_directory') . '/' . $filename ));
-        }
 
+            $user->setAvatar(new File($this->getParameter('upload_directory') . $this->getParameter('user_image_directory') . '/' . $filename ));
+        }
+        //dd($user);
         $form = $this->createForm(UserType::class, $user);
 
         $form->handleRequest($request);
@@ -67,6 +70,16 @@ class AjaxController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){
 
             $user = $form->getData();
+
+            //je ne fais le traitement que si une image a été envoyée
+            if($user->getImage()){
+                //je récupère le fichier
+                $file = $user->getImage();
+
+                $filename = $fileuploader->upload($file, $this->getParameter('user_image_directory'), $filename);
+            }
+
+            $user->setAvatar($filename);
 
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->persist($user);
@@ -76,7 +89,7 @@ class AjaxController extends AbstractController
 
         }
 
-        return $this->render("ajax/update_user.html.twig", ['userUpdateForm'=>$form->createView()]);
+        return $this->render("ajax/update_user.html.twig", ['userUpdateForm'=>$form->createView(), 'user'=>$user]);
     }
 
 
